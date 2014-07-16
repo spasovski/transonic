6ca294bf515e7f0f0e03574b731e95a69afe9325
Kevin Ngo <ngoke@mozilla.com>
2014-07-16 11:13:45 -0700

    remove dragging classes ondrop

--- src/media/js/views/feed_builder.js
+++ src/media/js/views/feed_builder.js
@@ -64,7 +64,9 @@ define('views/feed_builder', ['forms_transonic', 'jquery', 'jquery-sortable', 'f
         $feed.find('.empty-results').hide();
         $feed.find('.feed-elements').append($feed_element.clone());
         $('.feed-elements').sortable({
-            onDrop: function() {
+            onDrop: function($item) {
+                $item.removeClass('dragged').removeAttr('style')
+                z.body.removeClass('dragging');
                 z.page.trigger('refresh_preview');
             }
         });
